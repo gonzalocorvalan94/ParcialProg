2afcc92188daaae555a8409b65e76b96a5ba88da
Gaspar Lazarte <lazartegaspar@gmail.com>
2025-09-27 00:29:00 -0300

    Validadores

--- src/validators/validators.libros.js
+++ src/validators/validators.libros.js
@@ -1,25 +1,19 @@
 // validadores
 // el validador retornara true o false
+import chalk from "chalk"
 export function validarTitulo(titulo) {
-	if (!titulo || typeof titulo != "string" || libro.id <= 0) {
+	let clean = titulo.trim()
+	if (!clean || typeof clean != "string") {
 		console.error("Titulo no valido")
 		return false
 	}
 	return true
-	// if (!libro.titulo || typeof libro.titulo != "string") {
-	// 	err.push("Titulo no valido")
-	// }
-	// if (!libro.autor || typeof libro.autor != "string") {
-	// 	err.push("Autor no valido")
-	// }
-	// if (!libro.genero || typeof libro.genero != "string") {
-	// 	err.push("Genero no valido")
-	// }
-
-	// if (err.length != 0) {
-	// 	console.error("Error/es: " + err.join(" | "))
-	// 	return false
-	// } else {
-	// 	return true
-	// }
+}
+export function validarAutor(titulo) {
+	let clean = titulo.trim()
+	if (!clean || typeof clean != "string" || clean.length < 3) {
+		console.error("Autor no valido")
+		return false
+	}
+	return true
 }

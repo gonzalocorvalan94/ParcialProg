9aca602abe8b045a3e490a7acbcf737be09446bd
Gaspar Lazarte <lazartegaspar@gmail.com>
2025-09-26 23:56:35 -0300

    Creacion de modelo de libro y comienzo de validadores

--- src/service/service.libros.js
+++ src/service/service.libros.js
@@ -1,1 +1,11 @@
 //logica del crud
+import { Libro } from "../model/libros.models.js"
+import { validarTitulo } from "../validators/validators.libros.js"
+import fs from "fs"
+import path from "path"
+// funcion para leer el archivo. Devuelve todo el objeto del archivo data.json
+function read() {
+	const ruta = path.join(__dirname, "..", "db", "data.json")
+	const data = fs.readFileSync(ruta, "utf-8")
+	return data
+}
--- src/validators/validators.libros.js
+++ src/validators/validators.libros.js
@@ -1,1 +1,25 @@
 // validadores
+// el validador retornara true o false
+export function validarTitulo(titulo) {
+	if (!titulo || typeof titulo != "string" || libro.id <= 0) {
+		console.error("Titulo no valido")
+		return false
+	}
+	return true
+	// if (!libro.titulo || typeof libro.titulo != "string") {
+	// 	err.push("Titulo no valido")
+	// }
+	// if (!libro.autor || typeof libro.autor != "string") {
+	// 	err.push("Autor no valido")
+	// }
+	// if (!libro.genero || typeof libro.genero != "string") {
+	// 	err.push("Genero no valido")
+	// }
+
+	// if (err.length != 0) {
+	// 	console.error("Error/es: " + err.join(" | "))
+	// 	return false
+	// } else {
+	// 	return true
+	// }
+}

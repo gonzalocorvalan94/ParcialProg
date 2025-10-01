f4745d533f1a98bc96db70c23e559a5b12090f92
Serena <rotondaserena3@gmail.com>
2025-10-01 17:06:16 -0300

    agrego data.json y actualizo data.json

--- src/model/fileManager.js
+++ src/model/fileManager.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const filepath = path.join(__dirname, "libros.json"); //(__dirname,'nombre_de_la_db')
+const filepath = path.join(__dirname, "data.json"); //(__dirname,'nombre_de_la_db')
 
 export function leerDatos() {
   try {

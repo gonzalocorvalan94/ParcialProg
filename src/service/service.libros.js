a2f87135c9f13a142d055e0e4197a0e9b8594f76
Gonzalo Corvalan <gonzalo.corvalan77@gmail.com>
2025-10-03 08:35:00 -0300

    Comienzo de la estructura de service usuarios

--- src/service/service.libros.js
+++ src/service/service.libros.js
@@ -13,3 +13,40 @@ import {
 import fs from "fs"
 import path from "path"
 // funcion para leer el archivo. Devuelve todo el objeto del archivo data.json
+
+
+
+//usuarios
+
+function listarLibros(){
+//esto es un get
+}
+
+function consultarPorNombre(){
+//esto es un get por nombre
+}
+
+function crearPrestamo(){
+//post por nombre
+}
+
+function devolverLibro(){
+//delete por id
+}
+
+//admin
+
+//listar libros, pero ya se gestiona en usuarios
+
+function agregarLibro(){
+//post 
+}
+
+function modificarLibro(){
+//put por id
+}
+
+function eliminarLibro(){
+  //delete por id
+}
+
